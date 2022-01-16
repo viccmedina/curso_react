d4b09cbd518f5da70e49dd470a5a571a076bb1c9
Victoria <medina.vicc@gmail.com>
2022-01-16 17:45:48 -0300

    primer componente

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import  ReactDOM from 'react-dom'
 
-const App = <h1>HOLA REACT!</h1>
+// creamos un componente
+const TarjetaFruta = () => (
+<div>
+      <h3>Titulo</h3>
+      <hr/>
+      <p>Descripcion ........</p>
+  </div>
+)
+  
+
+const App = () => (
+    <div>
+        <TarjetaFruta/>
+        <TarjetaFruta></TarjetaFruta>
+    </div>
+)
+
 const root = document.getElementById('root')
-ReactDOM.render(App, root)
+ReactDOM.render(<App />, root)
